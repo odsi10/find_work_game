28691df1e7231bd811aa637f7db1b0ecd7cacdef
wa-i <ie-i>
2021-09-17 22:42:19 +0900

    ライトと軸

--- GameTemplate/Game/SkinModelRender.cpp
+++ GameTemplate/Game/SkinModelRender.cpp
@@ -14,7 +14,9 @@ bool SkinModelRender::Start()
 	return true;
 }
 
-void SkinModelRender::Init(const char* filePath)
+void SkinModelRender::Init(const char* filePath,
+	enModelUpAxis::EnModelUpAxis modelUpAxis
+)
 {
 	//tkmファイルのファイルパスを保持
 	m_tkmFilePath = filePath;
@@ -26,18 +28,129 @@ void SkinModelRender::Init(const char* filePath)
 	m_finishInit = true;
 }
 
-void SkinModelRender::InitModel(const char* filePath)
+void SkinModelRender::InitModel(const char* filePath,
+	enModelUpAxis::EnModelUpAxis modelUpAxis
+)
 {
+	//ライトの設定をする。
+	//ディレクションライトを初期化する
+	InitDirectionLight();
+
+	//ポイントライトを初期化する
+	//InitPointLight();
+
+	//スポットライトを初期化する
+	//InitSpotLight();
+
+	//環境光を初期化する
+	//InitAmbientLight();
+
+	//半球ライトを初期化する
+	//InitHemiLight();
 
 	//3Dモデルをロードするための情報を設定する
 	ModelInitData modelInitData;
+	
 	//tkmファイルのファイルパスを設定
 	modelInitData.m_tkmFilePath = filePath;
+	
 	//使用するシェーダーファイルパスを設定
 	modelInitData.m_fxFilePath = "Assets/shader/model.fx";
 
+	//ライトの情報を定数バッファとしてディスクリプタヒープに
+	//登録するためにモデルの初期化情報として渡す。
+	modelInitData.m_expandConstantBuffer = &m_light;
+	modelInitData.m_expandConstantBufferSize = sizeof(m_light);
+
 	//初期化情報を使ってモデル表示処理を初期化する
 	m_model.Init(modelInitData);
+}
+
+//////////////////////////////
+// ライトごとの初期化
+//////////////////////////////
+
+void SkinModelRender::InitDirectionLight()
+{
+	//ライトは斜め上から当たっている。
+	m_light.dirDirection.x = 1.0f;
+	m_light.dirDirection.y = -1.0f;
+	m_light.dirDirection.z = -1.0f;
+	//正規化する。
+	m_light.dirDirection.Normalize();
+	//ライトのカラーの設定（ライトの強さ）
+	m_light.dirColor.x = 1.5f;
+	m_light.dirColor.y = 1.5f;
+	m_light.dirColor.z = 1.5f;
+
+	//視点の位置を設定
+	m_light.eyePos = g_camera3D->GetPosition();
+}
+
+
+void SkinModelRender::InitPointLight()
+{
+	//ポイントライトの初期座標を設定する
+	m_light.ptPosition.x = 0.0f;
+	m_light.ptPosition.y = 50.0f;
+	m_light.ptPosition.z = 50.0f;
+
+	//ポイントライトの初期カラーを設定する
+	m_light.ptColor.x = 15.0f;
+	m_light.ptColor.y = 0.0f;
+	m_light.ptColor.z = 240.0f;
+
+	//ポイントライトの影響範囲を設定する
+	m_light.ptRange = 100.0f;
+}
+
+void SkinModelRender::InitSpotLight()
+{
+	//初期座標
+	m_light.spPosition.x = 0.0f;
+	m_light.spPosition.y = 50.0f;
+	m_light.spPosition.z = -150.0f;
+
+	//ライトのカラー
+	m_light.spColor.x = 200.0f;
+	m_light.spColor.y = 10.0f;
+	m_light.spColor.z = 10.0f;
+	//初期方向は斜め下にする。
+	m_light.spDirection.x = 1.0f;
+	m_light.spDirection.y = -1.0f;
+	m_light.spDirection.z = 1.0f;
+	//正規化
+	m_light.spDirection.Normalize();
+	//射出範囲は300
+	m_light.spRange = 300.0f;
+	//射出角度は25度
+	m_light.spAngle = Math::DegToRad(25.0f);
+}
+
+void SkinModelRender::InitAmbientLight()
+{
+	//環境光
+	m_light.ambientLight.x = 0.05f;
+	m_light.ambientLight.y = 0.05f;
+	m_light.ambientLight.z = 0.05f;
+}
+
+void SkinModelRender::InitHemiLight()
+{
+	//地面色、天球色、地面の法線のデータを設定する
+	m_light.groundColor.x = 0.7f;
+	m_light.groundColor.y = 0.5f;
+	m_light.groundColor.z = 0.3f;
+
+	//天球色を設定
+	m_light.skyColor.x = 0.15f;
+	m_light.skyColor.y = 0.7f;
+	m_light.skyColor.z = 0.95f;
+
+	//地面の法線を設定
+	m_light.groundNormal.x = 0.0f;
+	m_light.groundNormal.y = 1.0f;
+	m_light.groundNormal.z = 0.0f;
 }
 
 ////////////////////////////////////////////////////////////
--- GameTemplate/Game/SkinModelRender.h
+++ GameTemplate/Game/SkinModelRender.h
@@ -5,6 +5,45 @@
 
 #include"Name.h"
 
+//ライト用の構造体を定義する
+struct Light {
+	//ディレクションライト
+	Vector3 dirDirection; //ライトの方向
+	//C++側にはパディングを埋めておく。
+	float pad;
+	Vector3 dirColor;
+	float dirPad;
+
+	//ポイントライト
+	Vector3 ptPosition; //位置
+	float ptPad;			//パディング
+	Vector3 ptColor;	//カラー
+	float ptRange;		//影響範囲
+
+	//スポットライト
+	Vector3 spPosition;		//位置
+	float spPad;				//パディング
+	Vector3 spColor;		//カラー
+	float spRange;			//影響範囲
+	Vector3 spDirection;	//射出方向
+	float spAngle;			//射出角度
+
+	//構造体に視点の位置を追加する
+	Vector3 eyePos; //視点の位置
+	float eyePad;
+
+	//環境光
+	Vector3 ambientLight;   // アンビエントライト
+	float ambPad;
+
+	//半球ライト
+	Vector3 groundColor;	//地面色
+	float groPad;
+	Vector3 skyColor;		//天球色
+	float skyPad;
+	Vector3 groundNormal;	//地面の法線
+};
+
 class SkinModelRender : public IGameObject
 {
 public:
@@ -19,20 +58,31 @@ public:
 	 * @brief 初期化関数
 	 * @param filePath tkmファイルのファイルパス
 	*/
-	void Init(const char* filePath);
+	void Init(const char* filePath,
+		enModelUpAxis::EnModelUpAxis modelUpAxis = enModelUpAxis::enModelUpAxisZ
+	);
 
 private:
 	/**
 	 * @brief モデルの初期化
 	 * @param filePath tkmファイルのファイルパス
 	*/
-	void InitModel(const char* filePath);
+	void InitModel(const char* filePath,
+		enModelUpAxis::EnModelUpAxis = enModelUpAxis::enModelUpAxisZ
+	);
 
+	void InitDirectionLight(); //ディレクションライト
+	void InitPointLight(); //ポイントライト
+	void InitSpotLight(); //スポットライト
+	void InitAmbientLight(); //環境光
+	void InitHemiLight(); //半球ライト
 
 private: //データメンバ
 	Model m_model;
 
 	const char* m_tkmFilePath = nullptr; //tkmファイルのファイルパス
+
+	Light m_light;
 
 	Vector3 m_position = g_vec3Zero;			//位置
 	Quaternion m_rotation = g_quatIdentity;		//回転
